7fd958ce3936114f24946f2330078b060d2b6223
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-12-07 12:43:46 +0900

    Chore: Convert settings.gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://plugins.gradle.org/m2/")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+rootProject.name = "Neumorphism"
+
+include(":app")
+include(":neumorphic")
