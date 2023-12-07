d090d793531612eb59f8cf47ed3a37f4a4abe8af
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-12-07 15:51:44 +0900

    Feat: Multiplatform Resource Loading
    + Android Preview

--- app/src/androidMain/kotlin/com/gandiva/neumorphism/MainActivity.kt
+++ app/src/androidMain/kotlin/com/gandiva/neumorphism/MainActivity.kt
@@ -1,18 +1,88 @@
 package com.gandiva.neumorphism
 
+import App
+import CheckBoxAndRadioButtons
+import CircleActionButton
+import FlatButton
+import FlatSlider
+import InputBoxWithCardWrapper
+import PlainInputBox
+import PressedButton
+import PressedSlider
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import com.gandiva.neumorphic.shape.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             App()
         }
     }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
+
+
+@Preview
+@Composable
+fun InputBoxWithCardWrapperPreview() {
+    InputBoxWithCardWrapper()
+}
+
+
+@Preview
+@Composable
+fun PlainInputBoxPreview() {
+    PlainInputBox()
+}
+
+
+@Preview
+@Composable
+fun CheckBoxAndRadioButtonsPreview() {
+    CheckBoxAndRadioButtons()
+}
+
+
+@Preview
+@Composable
+fun PressedSliderPreview() {
+    PressedSlider()
+}
+
+
+@Preview
+@Composable
+fun FlatSliderPreview() {
+    FlatSlider()
+}
+
+
+@Preview
+@Composable
+fun PressedButtonPreview() {
+    PressedButton()
+}
+
+
+@Preview
+@Composable
+fun FlatButtonPreview() {
+    FlatButton()
+}
+
+
+@Preview
+@Composable
+fun CircleActionButtonPreview() {
+    CircleActionButton()
 }
