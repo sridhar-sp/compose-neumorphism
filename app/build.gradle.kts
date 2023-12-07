304c70dd9319a685241575936a7b4a21c8ae5adb
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-12-07 14:08:18 +0900

    Chore: Convert Module-level build.gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    kotlin("android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetpack.compose)
+}
+
+android {
+    namespace = "com.gandiva.neumorphism"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.gandiva.neumorphism"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+
+    implementation(projects.neumorphic)
+
+    implementation(libs.compose.ui)
+    implementation(compose.runtime)
+    implementation(compose.foundation)
+    implementation(compose.material)
+    implementation(libs.compose.ui.tooling.preview)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.material)
+
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.ui.test.junit)
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation(libs.compose.ui.tooling.preview)
+}
